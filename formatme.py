9067320a09d1cf3e37ef5354ca166c0b046de17a
sudhansu <sudhansu@yelp.com>
2018-05-16 00:04:18 -0700

    fix for #13 and #10

--- formatme.py
+++ formatme.py
@@ -98,6 +98,32 @@ def single_line_if_else(matchedobj):
 def remove_trailing_newline(matchedobj):
     return '\n' + matchedobj.group(0).split('\n\n')[1]
 
+"""
+process single/double equals while ignoring comments.
+The comments generally start with `\\` or `/*`. Ignore
+those matches and process the rest.
+"""
+def process_equals(matchedobj):
+    stmt = matchedobj.group(0)
+    if stmt:
+        if '//' in stmt or '/*' in stmt:
+            return stmt
+        elif stmt.count('=') == 1:
+            return ' = '
+        elif stmt.count('=') == 2:
+            return ' == '
+
+"""
+process divide by & equals while ignoring comments.
+"""
+def process_divide_equals(matchedobj):
+    stmt = matchedobj.group(0)
+    if stmt:
+        if '//' in stmt or '/*' in stmt:
+            return stmt
+        else:
+            return ' /= '
+
 regex_dict = OrderedDict([
     ###### RULE #######                                                             ###### DOCUMENTATION ######
     (r'^\s*(if|else)[^;{]+(;)', single_line_if_else),                               #1)  single line if/else should be encased with curly braces
@@ -112,46 +138,40 @@ regex_dict = OrderedDict([
     # (r'} *', r'}'),                                                               #9)  ?
     (r'(\, *[^\'\,\'|\w|\n|\(])', process_comma),                                   #10) 1 space after `, `
     (r', *\n', r', \n'),                                                            #11) no trailing space after `, `
-    (r' *= *', r' = '),                                                             #12) 1 space around ` = `
-    (r' *= *= *', r' == '),                                                         #13a) ` == `
+    (r'\/\*[\s\S]*?\*\/|\/\/[\s\S].*|\s*=\s*', process_equals),                     #12) 1 space around ` = `
+    (r'\/\*[\s\S]*?\*\/|\/\/[\s\S].*|\s*=\s*=\s*', process_equals),                 #13a) ` == `
     (r' *\+ *= *', r' += '),                                                        #13b) ` += `
     (r' *\- *= *', r' -= '),                                                        #13c) ` -= `
     (r' *\* *= *', r' *= '),                                                        #13d) ` *= `
     (r' *= *> *', r' => '),                                                         #13e) ` => `
-    (r' *\/ *= *', r' /= '),                                                        #13f) ` /= `
+    (r'\/\*[\s\S]*?\*\/|\/\/[\s\S].*| *\/ *= *', process_divide_equals),            #13f) ` /= `
     (r' *\! *= *', r' != '),                                                        #13g) ` != `
     (r' *> *= *', r' >= '),                                                         #13h) ` >= `
     (r' *< *= *', r' <= '),                                                         #13i) ` <= `
     (r' *& *= *', r' &= '),                                                         #13j) ` &= `
-    #(r' +\+ +', r' + '),                                                           #14) ?
-    #(r' +\- +', r' - '),                                                           #15) ?
+    (r' *\| *= *', ' |= '),                                                         #13k) ` |= `
+    #(r' +\+ +', r' + '),                                                           #14) `+`
+    #(r' +\- +', r' - '),                                                           #15) `-`
     (r' *\+\+ *', r'++'),                                                           #16) no space around `++`
     (r' *\-\- *', r'--'),                                                           #17) no space around `--`
-    # (r' +\* +', r' * '),                                                          #18) ? -- this is conflicting with `/**`
     # (r'\/\/ *', r'// '),                                                          #19) 1 space after `// ` comments
-    (r' *\=\> *', r' => '),                                                         #20) 1 space around ` => `
-    (r' *\=\= *', r' == '),                                                         #21) 1 space around ` == `
-    (r' *\+\= *', r' += '),                                                         #22) 1 space around ` += `
-    (r' *\-\= *', r' -= '),                                                         #23) 1 space around ` -= `
-    (r' *\*\= *', r' *= '),                                                         #24) 1 space around ` *= `
-    (r' */= *', r' /= '),                                                           #25) 1 space around ` /= `
-    (r'\n{2, }', r'\n\n'),                                                          #26) at most 2 newlines
-    (r' *; *\n', r';\n'),                                                           #27) no spaces around `;`
-    (r' +$', ''),                                                                   #28) no trailing whitespaces
-    (r'(.+) testMethod (.+)', remove_test_method),                                  #29) replace `testMethod` with `@isTest`
-    (r'(.+) class (.+) *{', class_name),                                            #30) 1 space between `SampleClass {`
-    (r'(.+)(\s*==\s*true|\s*!=\s*false)(.+)', process_if_true),                     #31) remove `== true` or `!= false`
-    #(r'((\w|\.)+|(\((\w|,)*\)))+\s*==\s*false', process_if_false),                 #32) convert `x == false` to `!x`
-    (r'^ *(for|if|while)[^{|}]+{$', process_multiline_loop),                          #33) 1 newline between multiline forloop and `{`
-    (r'(for|if|while) *\(.+\)\n+ *{', process_singleline_loop),                     #34) no newline between singline forloop and `{`
-    (r'(?i)\bSELECT\b *' , r'select '),                                             #35) lowercase soql keyword `select`
-    (r'(?i)\bFROM\b *' , r'from '),                                                 #36) lowercase soql keyword `from`
-    (r'(?i)\bWHERE\b *' , r'where '),                                               #37) lowercase soql keyword `where`
-    (r'(?i)\bLIMIT\b *' , r'limit '),                                               #38) lowercase soql keyword `limit`
-    (r'(?i)\bGROUP BY\b *' , r'group by '),                                         #40) lowercase soql keyword `group by`
-    (r'(?i)\bORDER BY\b *' , r'order by '),                                         #41) lowercase soql keyword `order by`
-    (r'(?i)\bHAVING\b *' , r'having '),                                             #42) lowercase soql keyword `having`
-    (r'\n{2}\s*}', remove_trailing_newline),                                        #43) remove trailing newline at end of functions
+    (r'\n{2, }', r'\n\n'),                                                          #20) at most 2 newlines
+    (r' *; *\n', r';\n'),                                                           #21) no spaces around `;`
+    (r' +$', ''),                                                                   #22) no trailing whitespaces
+    (r'(.+) testMethod (.+)', remove_test_method),                                  #23) replace `testMethod` with `@isTest`
+    (r'(.+) class (.+) *{', class_name),                                            #24) 1 space between `SampleClass {`
+    (r'(.+)(\s*==\s*true|\s*!=\s*false)(.+)', process_if_true),                     #25) remove `== true` or `!= false`
+    #(r'((\w|\.)+|(\((\w|,)*\)))+\s*==\s*false', process_if_false),                 #26) convert `x == false` to `!x`
+    (r'^ *(for|if|while)[^{|}]+{$', process_multiline_loop),                        #27) 1 newline between multiline forloop and `{`
+    (r'(for|if|while) *\(.+\)\n+ *{', process_singleline_loop),                     #28) no newline between singline forloop and `{`
+    (r'(?i)\bSELECT\b *' , r'select '),                                             #29) lowercase soql keyword `select`
+    (r'(?i)\bFROM\b *' , r'from '),                                                 #30) lowercase soql keyword `from`
+    (r'(?i)\bWHERE\b *' , r'where '),                                               #31) lowercase soql keyword `where`
+    (r'(?i)\bLIMIT\b *' , r'limit '),                                               #32) lowercase soql keyword `limit`
+    (r'(?i)\bGROUP BY\b *' , r'group by '),                                         #33) lowercase soql keyword `group by`
+    (r'(?i)\bORDER BY\b *' , r'order by '),                                         #34) lowercase soql keyword `order by`
+    (r'(?i)\bHAVING\b *' , r'having '),                                             #35) lowercase soql keyword `having`
+    (r'\n{2}\s*}', remove_trailing_newline),                                        #36) remove trailing newline at end of functions
 ])
 
 class FormatmeCommand(sublime_plugin.TextCommand):
@@ -166,25 +186,25 @@ class FormatmeCommand(sublime_plugin.TextCommand):
         else:
             process_selection(self, edit) # format only the selected text
 
-def process_whole_file(self, edit):
-    # select all text
-    region = sublime.Region(0, self.view.size())
-    text = self.view.substr(region)
-    formatMe(self, edit, region, text)
+    def process_whole_file(self, edit):
+        # select all text
+        region = sublime.Region(0, self.view.size())
+        text = self.view.substr(region)
+        formatMe(self, edit, region, text)
 
-def process_selection(self, edit):
-    # get user selection
-    for region in self.view.sel():
-        # if selection not empty then
-        if not region.empty():
-            text = self.view.substr(region)
-            formatMe(self, edit, region, text)
+    def process_selection(self, edit):
+        # get user selection
+        for region in self.view.sel():
+            # if selection not empty then
+            if not region.empty():
+                text = self.view.substr(region)
+                formatMe(self, edit, region, text)
 
-def formatMe(self, edit, region, text):
-    for key, value in regex_dict.items():
-        text = re.sub(key, value, text, flags=re.MULTILINE)
-    # replace content in view while removing any trailing whitespaces.
-    self.view.replace(edit, region, text.rstrip(' +'))
+    def formatMe(self, edit, region, text):
+        for key, value in regex_dict.items():
+            text = re.sub(key, value, text, flags=re.MULTILINE)
+        # replace content in view while removing any trailing whitespaces.
+        self.view.replace(edit, region, text.rstrip(' +'))
 
 class RemoveDirty(sublime_plugin.EventListener):
     # "save" event hook to remove dirty window
