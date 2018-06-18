930f186c7f6016c6d4c4ddea6bbcc8573d7df970
sudhansu <sudhansu@yelp.com>
2018-06-17 22:59:04 -0700

    testing phase 1/26 bug fixes

--- indentme.py
+++ indentme.py
@@ -8,6 +8,8 @@ def run(text):
     el_lazo_death_count = 0
     akane_no_mai_count = 0
     paren_ragnarok_count = 0
+    parenthesis_diff_count = 0
+    parenthesis_tabs_count = 0
 
     indent = ''
     newtext = ''
@@ -38,7 +40,7 @@ def run(text):
 
         is_comment = is_line_comment(line)
         open_parenthesis,close_parenthesis = is_parenthesis(line)
-
+        # soql
         if soql_flag:
             indent = soql_start_indent
         elif soql_end_flag:
@@ -49,55 +51,123 @@ def run(text):
         else:
             indent = tab_space*tabs
 
+        # } and if/else/for
         if '}' in line and not soql_flag and akane_no_mai(line):
             tabs -= line.count('}')
             indent = tab_space*tabs
             akane_no_mai_count = open_parenthesis - close_parenthesis
             tabs += akane_no_mai_count
             abra_ca_dabra(el_lazo_death_count, tabs, 1)
+        # multiline return start
         elif 'return' in line and line[-1] != ';':
             other_flag = True
             tabs += 1
             abra_ca_dabra(el_lazo_death_count, tabs, 2)
+        # multiline return end
         elif other_flag and ';' in line:
             tabs -= 1
             abra_ca_dabra(el_lazo_death_count, tabs, 3)
             other_flag = False
-        elif not soql_flag and akane_no_mai(line):
-            akane_no_mai_count = open_parenthesis - close_parenthesis
-            tabs += akane_no_mai_count
+        # multiline if/else if/for start
+        elif (
+            not soql_flag
+            and not other_flag
+            and akane_no_mai(line)
+        ):
+            diff = (open_parenthesis-close_parenthesis)
+            parenthesis_diff_count += diff
+            if akane_no_mai_flag:
+                akane_no_mai_count += diff
+            else:
+                akane_no_mai_count = diff
+            if akane_no_mai_count > 0:
+                parenthesis_tabs_count += 1
+                tabs += 1
+            elif akane_no_mai_count < 0:
+                tabs -= 1
+                parenthesis_tabs_count -= 1
+            if start_soql_query(line):
+                parenthesis_diff_count = 0
+                parenthesis_tabs_count = 0
+            print('in 4 diff = ' + str(parenthesis_diff_count) + ' tabs = ' + str(parenthesis_tabs_count))
             abra_ca_dabra(el_lazo_death_count, tabs, 4)
             akane_no_mai_flag = True
         elif (
+            '));' == line
+            or ');' == line
+            or '});' == line
+            or ')' == line
+        ):
+            paren_ragnarok_flag = False
+            akane_no_mai_count -= line.count(')')
+            # parenthesis_diff_count -= 1
+            print('in 10 diff = ' + str(parenthesis_diff_count) + ' tabs = ' + str(parenthesis_tabs_count))
+            # if line == '});':
+            #     tabs -= 2
+            #     parenthesis_diff_count = 0
+            #     parenthesis_tabs_count = 0
+            if ';' in line and parenthesis_tabs_count != 0:
+                tabs -= parenthesis_tabs_count
+                parenthesis_diff_count = 0
+                parenthesis_tabs_count = 0
+            elif ';' in line and parenthesis_tabs_count == 0:
+                parenthesis_diff_count = 0
+                parenthesis_tabs_count = 0
+            else:
+                tabs -= 1
+                parenthesis_diff_count -= 1
+                parenthesis_tabs_count -= 1
+            akane_no_mai_flag = False
+            indent = tab_space*tabs
+            abra_ca_dabra(el_lazo_death_count, tabs, 10)
+        # multiline if/else if/for start
+        elif (
             not soql_flag
-            and line[-1] == ')'
+            and not other_flag
+            #and line[-1] == ')'
             and akane_no_mai_flag
+            and not return_of_parenthesis(line)
             and close_parenthesis > open_parenthesis
         ):
             open_count, close_count = is_parenthesis(line)
             diff_count = close_count-open_count
+            parenthesis_diff_count += open_count - close_count
             if paren_ragnarok_flag:
                 if (
                     diff_count > 0 and
                     diff_count < (akane_no_mai_count + paren_ragnarok_count)
                 ):
-                    tabs -= diff_count
+                    tabs -= 1
+                    parenthesis_tabs_count -= 1
                 else:
-                    tabs -= (akane_no_mai_count + paren_ragnarok_count)
+                    tabs -= 2
+                    parenthesis_tabs_count -= 2
                 paren_ragnarok_count = 0
                 paren_ragnarok_flag = False
-            else:
-                tabs -= akane_no_mai_count
-            akane_no_mai_count = 0
-            akane_no_mai_flag = False
-            if line == ')':
+            elif parenthesis_tabs_count > 0:
+                tabs -= 1
+                parenthesis_tabs_count -= 1
+            akane_no_mai_count += open_count - close_count
+            if akane_no_mai_count == 0:
+                akane_no_mai_flag = False
+            # if line == ')':
+            if ';' in line:
                 indent = tab_space*tabs
+            if (
+                parenthesis_diff_count == 0
+                and parenthesis_tabs_count != 0
+            ):
+                tabs -= parenthesis_tabs_count
+                parenthesis_tabs_count = 0
+            print('in 5 diff = ' + str(parenthesis_diff_count) + ' tabs = ' + str(parenthesis_tabs_count))
             abra_ca_dabra(el_lazo_death_count, tabs, 5)
+        # )] and ]; with multiline if/else/for
         elif les_ecorchés(line) and akane_no_mai_flag:
-            tabs -= akane_no_mai_count
+            tabs -= 1
             akane_no_mai_count = 0
             abra_ca_dabra(el_lazo_death_count, tabs, 6)
             akane_no_mai_flag = False
+        # multiline ) statement end
         elif (
             not soql_flag
             and not other_flag
@@ -105,6 +175,7 @@ def run(text):
         ):
             tabs -= 1
             abra_ca_dabra(el_lazo_death_count, tabs, 7)
+        # multiline statement start
         elif (
             not other_flag
             and not soql_flag
@@ -120,8 +191,13 @@ def run(text):
             curly_diff = curly_contrapasso(line)
             if curly_diff != 0:
                 tabs += curly_diff
-            tabs += paren_count
+            # tabs += paren_count
+            if paren_count > 0:
+                tabs += 1
+            elif paren_count < 0:
+                tabs -= 1
             abra_ca_dabra(el_lazo_death_count, tabs, 8)
+        # multiline statement end
         elif (
             not other_flag
             and paren_ragnarok_flag
@@ -129,19 +205,18 @@ def run(text):
         ):
             paren_ragnarok_flag = False
             curly_diff = curly_contrapasso(line)
-            if curly_diff != 0:
-                tabs += curly_diff
-            tabs -= paren_ragnarok_count
+            # if curly_diff != 0:
+            #     tabs += curly_diff
+            # tabs -= paren_ragnarok_count
+            total_diff = curly_diff + paren_ragnarok_count
+            if total_diff > 0:
+                tabs += 1
+            elif total_diff < 0:
+                tabs -= 1
             if line == '});':
                 indent = tab_space*tabs
             paren_ragnarok_count = 0
             abra_ca_dabra(el_lazo_death_count, tabs, 9)
-        elif ');' == line or ')' == line:
-            tabs -= 1
-            paren_ragnarok_flag = False
-            indent = tab_space*tabs
-            akane_no_mai_flag = False
-            abra_ca_dabra(el_lazo_death_count, tabs, 10)
         elif '}' in line and '{' in line and line[0] == '}':
             indent = tab_space*(tabs-1)
             open_count = line.count('{')
@@ -157,11 +232,25 @@ def run(text):
         elif '{' in line:
             indent = tab_space*tabs
             tabs += line.count('{')
+            if akane_no_mai_flag:
+                parenthesis_diff_count += line.count('{')
+                parenthesis_tabs_count += line.count('{')
+            print('in 13 diff = ' + str(parenthesis_diff_count) + ' tabs = ' + str(parenthesis_tabs_count))
             abra_ca_dabra(el_lazo_death_count, tabs, 13)
         elif '}' in line:
             tabs -= line.count('}')
+            if akane_no_mai_flag:
+                open_count,close_count = is_parenthesis(line)
+                parenthesis_diff_count += open_count - close_count
+                parenthesis_tabs_count -= line.count('}')
+            print('in 14 diff = ' + str(parenthesis_diff_count) + ' tabs = ' + str(parenthesis_tabs_count))
             indent = tab_space*tabs
             abra_ca_dabra(el_lazo_death_count, tabs, 14)
+        # soql in the same line
+        elif start_soql_query(line) and end_soql_query(line):
+            indent = tab_space*tabs
+            soql_flag = False
+            abra_ca_dabra(el_lazo_death_count, tabs, 16)
         elif not soql_flag:
             indent = tab_space*tabs
             abra_ca_dabra(el_lazo_death_count, tabs, 15)
@@ -174,6 +263,9 @@ def run(text):
 
         newline = indent + line.rstrip()
         newtext += newline  + '\n'
+
+        if start_soql_query(line) and end_soql_query(line):
+            continue
 
         if start_soql_query(newline):
             # find the position in line
@@ -211,7 +303,7 @@ def run(text):
     # remove the last '\n'
     newtext = newtext[:-1]
     if tabs == 0:
-        print('\n🙀🐾😹If I fits, I sits😻🐾🐈')
+        print('\n🙀🐾If I fits, I sits🐾🐈')
     else:
         print('\n🏇🔫🤖violent delights have violent ends🤖🔫🏇')
     return newtext
@@ -226,6 +318,9 @@ def is_line_comment(line):
 
 def start_soql_query(line):
     return ': [' in line or '= [' in line or '([' in line
+
+def end_soql_query(line):
+    return '];' in line
 
 def is_parenthesis(line):
     open_count = line.count('(')
@@ -279,10 +374,15 @@ def elephant_stone_strip(word):
 def akane_no_mai(line):
     open_parenthesis,close_parenthesis = is_parenthesis(line)
     if (
-        ('if (' in line
+        (
+            'if (' in line
             or 'else if (' in line
             or 'for (' in line
             or line[-1] == '('
+            or (
+                parenthesis_ragnarok(line) > 0
+                and not return_of_parenthesis(line)
+            )
         )
         and open_parenthesis > close_parenthesis
     ):
@@ -329,7 +429,7 @@ def return_of_parenthesis(line):
     if (
         "'(" in line
         or "('" in line
-        or "')" in line
+        # or "')" in line
         or ")'" in line
     ):
         return True
